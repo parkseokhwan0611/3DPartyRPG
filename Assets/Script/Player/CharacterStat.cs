347d63b6a6f9d25ebd1f6e7de42d376cce6acc27
박석환 <papermaker@naver.com>
2026-05-02 16:28:53 +0900

    CharacterHP

--- Assets/Script/Player/CharacterStat.cs
+++ Assets/Script/Player/CharacterStat.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System; // Action을 사용하기 위해 필요
 
 public class CharacterStat : MonoBehaviour, IDamageable
 {
     [Header("# Base Stats")]
     public float maxHp;
     public float hp;
+    public event Action OnHpChanged;
+    public float maxMp;
+    public float mp;
     void Awake()
     {
         // 초기화 (StatManager에서 데이터를 받아올 수도 있음)
@@ -16,6 +20,7 @@ public class CharacterStat : MonoBehaviour, IDamageable
     public void TakeDamage(float damage, GameObject attacker)
     {
         hp -= damage;
+        OnHpChanged?.Invoke();
         if (hp <= 0) Die();
     }
 
---
+++ Assets/Script/UI/PartyHPUIScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PartyHPUIScript : MonoBehaviour
+{
+    public CharacterStat stat;
+    public Image hpBar;
+    
+    // 부드러운 연출을 위해 여전히 Lerp/MoveTowards를 쓴다면 코루틴이 효율적입니다.
+    private Coroutine hpCoroutine;
+
+    void OnEnable()
+    {
+        // 이벤트 구독 시작
+        stat.OnHpChanged += UpdateHpUI;
+    }
+
+    void OnDisable()
+    {
+        // 오브젝트가 꺼질 때 구독 해제 (메모리 누수 방지)
+        stat.OnHpChanged -= UpdateHpUI;
+    }
+
+    void UpdateHpUI()
+    {
+        // HP가 변했다는 신호를 받았을 때 실행됨
+        float targetFill = stat.hp / stat.maxHp;
+        
+        if (hpCoroutine != null) StopCoroutine(hpCoroutine);
+        hpCoroutine = StartCoroutine(SmoothUpdateBar(targetFill));
+    }
+
+    System.Collections.IEnumerator SmoothUpdateBar(float targetFill)
+    {
+        while (!Mathf.Approximately(hpBar.fillAmount, targetFill))
+        {
+            hpBar.fillAmount = Mathf.MoveTowards(hpBar.fillAmount, targetFill, Time.deltaTime * 1.5f);
+            yield return null;
+        }
+    }
+}
